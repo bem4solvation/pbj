dd7d6c8d93497ab445a647d6eea2917dbcc07868
Styles <kenneth.styles@sansano.usm.cl>
2021-09-21 16:10:09 -0300

    Just created the empty modules folders and init files, as discussed on the early september meeting

--- pbj/__init__.py
+++ pbj/__init__.py
@@ -1,8 +1,11 @@
 """Poisson Boltzmann & Jupyter: Bempp based biomolecular electrostatics solver."""
 
 # Add imports here
-from .electrostatics import *
-
+import bempp.api
+import os
+from pbj.electrostatics.solute import *
+from pbj.nonpolar import * 
+from pbj.mesh import *
 # Handle versioneer
 from ._version import get_versions
 versions = get_versions()
