aee7ea4a200e7014fbeec267255921c978399182
Miguel Godoy <magodoydiaz@gmail.com>
2021-10-02 17:42:28 -0300

    Added several functions on mesh

--- pbj/__init__.py
+++ pbj/__init__.py
@@ -6,9 +6,11 @@ import os
 from pbj.electrostatics.solute import *
 from pbj.nonpolar import * 
 from pbj.mesh import *
+PBJ_PATH = os.path.abspath(os.path.dirname(os.path.realpath(__file__))) #Agregamos PBJ_PATH
 # Handle versioneer
 from ._version import get_versions
 versions = get_versions()
 __version__ = versions['version']
 __git_revision__ = versions['full-revisionid']
 del get_versions, versions
+
