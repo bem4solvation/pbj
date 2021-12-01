a8f3b0a35452a18fcd0f5cb875c4cb31993f6e99
Stefan Search <stefan.search@gmail.com>
2021-11-30 21:40:00 -0300

    Correct init file for formulations module, in order to import all formulations.

--- pbj/electrostatics/pb_formulation/formulations/__init__.py
+++ pbj/electrostatics/pb_formulation/formulations/__init__.py
@@ -1,3 +1,13 @@
-import pbj.electrostatics.pb_formulation.formulations.direct
-import pbj.electrostatics.pb_formulation.formulations.juffer
-import pbj.electrostatics.pb_formulation.formulations.alpha_beta
+from . import direct
+from . import direct_permuted
+from . import direct_external
+from . import direct_external_permuted
+from . import juffer
+from . import lu
+from . import alpha_beta
+from . import alpha_beta_external_potential
+from . import alpha_beta_single_blocked
+from . import first_kind_internal
+from . import first_kind_external
+from . import muller_internal
+from . import muller_external
