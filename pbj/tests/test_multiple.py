ae19b920520f8e0ae56fe0ba69a126d80ecf6fc2
iaddison-smith <ian.addison-smith@sansano.usm.cl>
2026-05-16 17:47:46 -0400

    update pep8 scripts missed

--- pbj/tests/test_multiple.py
+++ pbj/tests/test_multiple.py
@@ -20,16 +20,20 @@ def test_multiple():
         print("Creating sphere meshes")
         pqrpath = os.path.join(PBJ_PATH, "tests", "spheres", "test_sphere1.pqr")
         for mesh_dens in [2, 4, 8, 16]:
-            sphere = pbj.implicit_solvent.Solute(pqrpath, mesh_density=mesh_dens, mesh_generator="msms")
+            sphere = pbj.implicit_solvent.Solute(
+                pqrpath, mesh_density=mesh_dens, mesh_generator="msms"
+            )
             spheres.append(sphere)
         return spheres
-    
+
     def spheres2():
         spheres = []
         print("Creating sphere meshes")
         pqrpath = os.path.join(PBJ_PATH, "tests", "spheres", "test_sphere2.pqr")
         for mesh_dens in [2, 4, 8, 16]:
-            sphere = pbj.implicit_solvent.Solute(pqrpath, mesh_density=mesh_dens, mesh_generator="msms")
+            sphere = pbj.implicit_solvent.Solute(
+                pqrpath, mesh_density=mesh_dens, mesh_generator="msms"
+            )
             spheres.append(sphere)
         return spheres
 
@@ -38,7 +42,9 @@ def test_multiple():
     spheres2 = spheres2()
     file = open("test_results_multiple.txt", "w")
     for j in range(len(spheres1)):
-        print(f'Calculating solvation energy/forces for mesh density {spheres1[j].mesh_density}')
+        print(
+            f"Calculating solvation energy/forces for mesh density {spheres1[j].mesh_density}"
+        )
         simulation = pbj.implicit_solvent.Simulation()
         simulation.add_solute(spheres_sing[j])
         simulation.calculate_solvation_energy()
@@ -47,22 +53,27 @@ def test_multiple():
         simulation_mult = pbj.implicit_solvent.Simulation()
         simulation_mult.add_solute(spheres1[j])
         simulation_mult.add_solute(spheres2[j])
-        simulation_mult.calculate_solvation_forces(force_formulation='maxwell_tensor')
-        f_solv_maxwell_tensor = spheres1[j].results['f_solv']
-        simulation_mult.calculate_solvation_forces(force_formulation='energy_functional')
-        f_solv_energy_func = spheres1[j].results['f_solv']
+        simulation_mult.calculate_solvation_forces(force_formulation="maxwell_tensor")
+        f_solv_maxwell_tensor = spheres1[j].results["f_solv"]
+        simulation_mult.calculate_solvation_forces(
+            force_formulation="energy_functional"
+        )
+        f_solv_energy_func = spheres1[j].results["f_solv"]
         simulation_mult.calculate_solvation_energy()
-        energy_val_multiple = spheres1[j].results["electrostatic_solvation_energy"] + \
-            spheres2[j].results["electrostatic_solvation_energy"]
-        
-        energy_val = energy_val_multiple - 2*energy_val_single
-        file.write(f"Mesh density: {spheres1[j].mesh_density}, Binding energy energy: {energy_val:.4f} " \
-                f"Solvation forces (Maxwell tensor): {np.linalg.norm(f_solv_maxwell_tensor):.4f} " \
-                f"Solvation forces (Energy functional): {np.linalg.norm(f_solv_energy_func):.4f} \n")
-        
+        energy_val_multiple = (
+            spheres1[j].results["electrostatic_solvation_energy"]
+            + spheres2[j].results["electrostatic_solvation_energy"]
+        )
+
+        energy_val = energy_val_multiple - 2 * energy_val_single
+        file.write(
+            f"Mesh density: {spheres1[j].mesh_density}, Binding energy energy: {energy_val:.4f} "
+            f"Solvation forces (Maxwell tensor): {np.linalg.norm(f_solv_maxwell_tensor):.4f} "
+            f"Solvation forces (Energy functional): {np.linalg.norm(f_solv_energy_func):.4f} \n"
+        )
 
     file.write("\n\nReference values (Paper JCTC 2023):\n")
-    file.write(f"Force value (by virtual work) (kcal/molA): 1.9425 \n") 
-    file.write(f"Binding energy value (kcal/mol): 3.9689 \n")   
-    
+    file.write(f"Force value (by virtual work) (kcal/molA): 1.9425 \n")
+    file.write(f"Binding energy value (kcal/mol): 3.9689 \n")
+
     file.close()
